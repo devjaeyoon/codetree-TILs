baebe8c275838dae9e93513541af6c24aacf579b
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-07 18:13:44 +0900

    Title: 함수를 이용한 소수 판별, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/함수를 이용한 소수 판별/decimal-decisions-using-functions.js
@@ -0,0 +1,26 @@
+function isPrime(num) {
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function solution (start, end) {
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        if (isPrime(i)) {
+            sum += i;
+        }
+    }
+
+    return sum;
+}
+
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+console.log(solution(a, b));
