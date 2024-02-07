6027a09902e203e0fbbefa95c8cf247acf31e2de
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-07 18:51:53 +0900

    Title: 함수를 이용한 온전수 판별, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240207/함수를 이용한 온전수 판별/determining-the-whole-number-using-a-function.js
@@ -0,0 +1,29 @@
+function isOnJeonSu(num) {
+    if (num % 2 === 0) {
+        return false;
+    }
+    if (num / 10 === 0.5) {
+        return false;
+    }
+    if (num % 3 === 0 && num % 9 !== 0) {
+        return false;
+    }
+    return true;
+}
+
+function solution(start, end) {
+    let cnt = 0;
+    
+    for (let i = start; i <= end; i++) {
+        if (isOnJeonSu(i)) {
+            cnt++;
+        }
+    }
+
+    return snt;
+}
+
+const fs = require('fs');
+const [a, b] = fs.readFileSync(0).toString().trim().split(' ').map(Number);
+
+console.log(solution(a, b));
