7b1fd7cf619d4e6deee374956a6ef35f22690a64
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-07 19:18:54 +0900

    Title: 2021년 날짜의 유무, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/2021년 날짜의 유무/with-or-without-2021.js
@@ -0,0 +1,77 @@
+function check2021Date(month, date) {
+    if (month === 1) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 2) {
+        if (date <= 28) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 3) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 4) {
+        if (date <= 30) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 5) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 6) {
+        if (date <= 30) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 7) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 8) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 9) {
+        if (date <= 30) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 10) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 11) {
+        if (date <= 30) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+    if (month === 12) {
+        if (date <= 31) {
+            return 'Yes'
+        }
+        return 'No';
+    }
+}
+
+const [M, D] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
+console.log(check2021Date(M, D));
