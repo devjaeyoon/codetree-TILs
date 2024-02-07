36e854b764ce3c3b149ff4b2b9c810505404f580
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-07 15:32:18 +0900

    Title: 정수의 최솟값, Time: 260ms, Memory: 14MB, Status: Passed - CodeTree

--- 240207/정수의 최솟값/minimum-value-of-an-integer.js
+++ 240207/정수의 최솟값/minimum-value-of-an-integer.js
@@ -17,6 +17,24 @@ function solution(num1, num2, num3) {
         }
         return num1;
     }
+    if (num2 <= num1) {
+        if (num2 <= num3) {
+            return num2;
+        }
+        return num3;
+    }
+    if (num3 <= num2) {
+        if (num3 <= num1) {
+            return num3;
+        }
+        return num1;
+    }
+    if (num3 <= num1) {
+        if (num3 <= num2) {
+            return num3;
+        }
+        return num2;
+    }
 }
 
 const fs = require('fs');
