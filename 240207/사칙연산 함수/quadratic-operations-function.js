2d6c62323d65f6cd9579df928e1670dcb641b6b4
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-07 18:46:04 +0900

    Title: 사칙연산 함수, Time: 166ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240207/사칙연산 함수/quadratic-operations-function.js
@@ -0,0 +1,23 @@
+function solution(num1, op, num2) {
+    let answer = ''
+
+    if (op === '+') {
+        answer += `${num1} ${op} ${num2} = ${Number(num1) + Number(num2)}`;
+    }
+    if (op === '-') {
+        answer += `${num1} ${op} ${num2} = ${Number(num1) - Number(num2)}`;
+    }
+    if (op === '/') {
+        answer += `${num1} ${op} ${num2} = ${Math.trunc(Number(num1) / Number(num2))}`;
+    }
+    if (op === '*') {
+        answer += `${num1} ${op} ${num2} = ${Number(num1) * Number(num2)}`;
+    }
+
+    return answer;
+}
+
+const fs = require('fs');
+const [a, o, c] = fs.readFileSync(0).toString().trim().split(' ');
+
+console.log(solution(a, o, c))
