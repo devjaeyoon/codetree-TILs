d7b4738e75110206be71c6b54f702d472b6c6b6f
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-11 02:51:22 +0900

    Title: 총합 구하기, Time: 189ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240211/총합 구하기/find-the-total.js
@@ -0,0 +1,11 @@
+const [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
+
+let sum = 0;
+
+for (let i = a; i <= b; i++) {
+    if (i % 6 === 0 && i % 8 !== 0) {
+        sum += i;
+    }
+}
+
+console.log(sum);
