1eb0d1449050622127ee4f83751e56e4876ab2d8
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-11 02:58:21 +0900

    Title: 조건에 부합하는 수, Time: 191ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240211/조건에 부합하는 수/number-that-meets-the-condition.js
@@ -0,0 +1,18 @@
+const a = Number(require('fs').readFileSync(0).toString().trim());
+
+let str = '';
+
+for (let i = 1; i <= a; i++) {
+    if (i % 2 === 0 && i % 4 !== 0) {
+        continue;
+    }
+    if (Math.floor(i / 8) % 2 === 0) {
+        continue;
+    }
+    if (i % 7 < 4) {
+        continue;
+    }
+    str += i + ' ';
+}
+
+console.log(str);
