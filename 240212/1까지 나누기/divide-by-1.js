e4470f606f88df8c61cf60403b063f028b068bb3
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-12 17:22:52 +0900

    Title: 1까지 나누기, Time: 297ms, Memory: 47MB, Status: Passed - CodeTree

--- 240212/1까지 나누기/divide-by-1.js
+++ 240212/1까지 나누기/divide-by-1.js
@@ -1,11 +1,11 @@
 let n = Number(require('fs').readFileSync(0).toString().trim());
 
+let divisor = 1;
 let cnt = 0;
-let i = 1;
 
-while (n >= 1) {
-    n = Math.floor(n / i);
-    i++;
+while (n > 1) {
+    n = Math.floor(n / divisor);
+    divisor++;
     cnt++;
 }
 
