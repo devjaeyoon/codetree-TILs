fdea4c3c881ba8046555672d08f3770adb33f09f
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-02-20 10:26:03 +0900

    Title: 선택 정렬 구현, Time: 193ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240220/선택 정렬 구현/implement-selection-sort.js
@@ -0,0 +1,28 @@
+const lines = require('fs').readFileSync(0).toString().trim().split('\n');
+const numbers = lines[1].split(' ').map(Number);
+
+function selectionSort(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length - 1; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        const tmp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = tmp;
+    }
+
+    return arr;
+}
+
+selectionSort(numbers);
+
+let str = '';
+
+for (let number of numbers) {
+    str += number + ' ';
+}
+
+console.log(str);
