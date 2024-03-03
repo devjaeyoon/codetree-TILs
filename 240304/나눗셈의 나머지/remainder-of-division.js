bdaca62e6e14f1abc25fa61a68a238ae893e3b6b
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-04 01:15:04 +0900

    Title: 나눗셈의 나머지, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240304/나눗셈의 나머지/remainder-of-division.js
@@ -0,0 +1,17 @@
+const [a, b] = require('fs').readFileSync(0).toString().trim().split(' ').map(Number);
+const rests = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+let now = a;
+
+while (now !== 0) {
+    rests[now % b]++;
+    now = Math.floor(now / b);
+}
+
+let sum = 0;
+
+for (let rest of rests) {
+    sum += Math.pow(rest, 2);
+}
+
+console.log(sum);
