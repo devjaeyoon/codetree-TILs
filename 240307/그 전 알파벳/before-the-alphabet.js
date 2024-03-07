ae72e68f4e06ec6807e872b7a20f804973dbd7e0
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-07 13:52:01 +0900

    Title: 그 전 알파벳, Time: 194ms, Memory: 14MB, Status: Passed - CodeTree

--- 240307/그 전 알파벳/before-the-alphabet.js
+++ 240307/그 전 알파벳/before-the-alphabet.js
@@ -1,2 +1,7 @@
 const input = require('fs').readFileSync(0).toString().trim();
-console.log(String.fromCharCode(input.charCodeAt(0) - 1));
+
+if (input === 'a') {
+    console.log('z');
+} else {
+    console.log(String.fromCharCode(input.charCodeAt(0) - 1));
+}
