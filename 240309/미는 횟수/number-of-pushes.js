35259841fafdadd8ba04472e743b5d51f36dafe7
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-09 01:40:10 +0900

    Title: 미는 횟수, Time: 172ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240309/미는 횟수/number-of-pushes.js
+++ 240309/미는 횟수/number-of-pushes.js
@@ -3,11 +3,12 @@ const B = lines[1];
 let A = lines[0];
 let cnt = 0;
 
-while (A !== B && cnt < A.length) {
+while (cnt < A.length) {
     A = A.split('');
     A.push(A.shift());
     A = A.join('');
     cnt += 1;
+    if (A === B) break;
 }
 
-console.log(cnt <= A.length ? cnt : -1);
+console.log(A === B ? cnt : -1);
