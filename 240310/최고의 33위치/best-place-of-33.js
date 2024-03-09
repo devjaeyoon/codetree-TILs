720249ee01e565fc4ac8f9b2327202eb81f13628
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-10 00:35:00 +0900

    Title: 최고의 33위치, Time: 177ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240310/최고의 33위치/best-place-of-33.js
@@ -0,0 +1,31 @@
+const lines = require('fs').readFileSync(0).toString().trim().split('\n');
+const N = Number(lines[0]);
+const arr = [];
+
+for (let i = 1; i <= N; i++) {
+  arr.push(lines[i].split(' ').map(Number));
+}
+
+let max = 0;
+
+for (let i = 0; i < N - 2; i++) {
+  let sum = 0;
+  for (let j = 0; j < N - 2; j++) {
+    sum +=
+      arr[i][j] +
+      arr[i + 1][j] +
+      arr[i + 2][j] +
+      arr[i][j + 1] +
+      arr[i + 1][j + 1] +
+      arr[i + 2][j + 1] +
+      arr[i][j + 2] +
+      arr[i + 1][j + 2] +
+      arr[i + 2][j + 2];
+  }
+
+  if (sum > max) {
+    max = sum;
+  }
+}
+
+console.log(max);
