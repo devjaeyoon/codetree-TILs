2ff84445274329dad406f7d5bbca8e209cf42493
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-10 01:27:09 +0900

    Title: 행복한 수열의 개수, Time: 220ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240310/행복한 수열의 개수/number-of-happy-sequence.js
@@ -0,0 +1,59 @@
+const lines = require('fs').readFileSync(0).toString().trim().split('\n');
+const [n, m] = lines[0].split(' ').map(Number);
+
+if (m === 1) {
+  console.log(2 * n);
+}
+
+const arr = [];
+let cnt = 0;
+
+for (let i = 1; i <= n; i++) {
+  const numbers = lines[i].split(' ');
+  const obj = {};
+  let lastNumStr = '0';
+
+  for (let number of numbers) {
+    if (number === lastNumStr) {
+      obj[lastNumStr] += 1;
+    } else {
+      lastNumStr = number;
+      obj[lastNumStr] = 1;
+    }
+  }
+
+  for (let value of Object.values(obj)) {
+    if (value >= m) {
+      cnt++;
+    }
+  }
+
+  arr.push(numbers);
+}
+
+for (let i = 0; i < n; i++) {
+  const numbers = [];
+  const obj = {};
+  let lastNumStr = '0';
+
+  for (let j = 0; j < n; j++) {
+    numbers.push(arr[j][i]);
+  }
+
+  for (let number of numbers) {
+    if (number === lastNumStr) {
+      obj[lastNumStr] += 1;
+    } else {
+      lastNumStr = number;
+      obj[lastNumStr] = 1;
+    }
+  }
+  
+  for (let value of Object.values(obj)) {
+    if (value >= m) {
+      cnt++;
+    }
+  }
+}
+
+console.log(cnt);
