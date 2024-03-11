c68626805920ef680fea9f2a9aa129f231bf1f4b
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-12 01:36:24 +0900

    Title: 이상한 진수 2, Time: 165ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240312/이상한 진수 2/awkward-digits-2.js
@@ -0,0 +1,29 @@
+const input = require('fs').readFileSync(0).toString().trim();
+const binNumbers = [];
+
+for (let i = 0; i < input.length; i++) {
+  let binNumberString = '';
+  for (let j = 0; j < i; j++) {
+    binNumberString += input[j];
+  }
+  if (input[i] === '0') {
+    binNumberString += '1';
+    for (let j = i + 1; j < input.length; j++) {
+      binNumberString += input[j];
+    }
+  } else {
+    for (let j = i; j < input.length; j++) {
+      binNumberString += input[j];
+    }
+  }
+
+  binNumbers.push(binNumberString);
+}
+
+const decNumbers = [];
+
+for (let binNumber of binNumbers) {
+  decNumbers.push(parseInt(binNumber, 2));
+}
+
+console.log(Math.max(...decNumbers));
