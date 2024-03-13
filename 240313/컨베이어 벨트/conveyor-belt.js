a355958e9659d7d54fa096452ff5077a1e86bb63
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-13 15:04:41 +0900

    Title: 컨베이어 벨트, Time: 175ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240313/컨베이어 벨트/conveyor-belt.js
@@ -0,0 +1,21 @@
+const lines = require('fs').readFileSync(0).toString().trim().split('\n');
+const [n, t] = lines[0].split(' ').map(Number);
+const arr = [];
+
+for (let i = 1; i <= 2; i++) {
+  arr.push(...lines[i].split(' ').map(Number));
+}
+
+for (let i = 0; i < t; i++) {
+  arr.unshift(arr.pop());
+}
+
+for (let i = 0; i < arr.length; i += n) {
+  let str = '';
+
+  for (let j = 0; j < n; j++) {
+    str += arr[i + j] + ' ';
+  }
+
+  console.log(str);
+}
