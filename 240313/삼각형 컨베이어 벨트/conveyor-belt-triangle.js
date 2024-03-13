73d1ad5916c496886025654a4dbd6d9a2d33aef6
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-13 16:07:18 +0900

    Title: 삼각형 컨베이어 벨트, Time: 235ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240313/삼각형 컨베이어 벨트/conveyor-belt-triangle.js
@@ -0,0 +1,35 @@
+const lines = require('fs').readFileSync(0).toString().trim().split('\n');
+const [n, t] = lines[0].split(' ').map(Number);
+const arr = [];
+
+for (let i = 1; i <= 3; i++) {
+  arr.push(...lines[i].split(' ').map(Number));
+}
+
+for (let i = 0; i < t; i++) {
+  arr.unshift(arr.pop());
+}
+
+let str1 = '';
+
+for (let i = 0; i < n; i++) {
+  str1 += arr[i] + ' ';
+}
+
+console.log(str1);
+
+let str2 = '';
+
+for (let i = n; i < 2 * n; i++) {
+  str2 += arr[i] + ' ';
+}
+
+console.log(str2);
+
+let str3 = '';
+
+for (let i = 2 * n; i < 3 * n; i++) {
+  str3 += arr[i] + ' ';
+}
+
+console.log(str3);
