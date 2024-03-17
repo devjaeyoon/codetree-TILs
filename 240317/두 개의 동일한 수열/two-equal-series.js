81a185d37b20d4471927fa329120d6b02eb40e51
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-17 22:38:53 +0900

    Title: 두 개의 동일한 수열, Time: 162ms, Memory: 14MB, Status: Passed - CodeTree

--- 240317/두 개의 동일한 수열/two-equal-series.js
+++ 240317/두 개의 동일한 수열/two-equal-series.js
@@ -1,7 +1,7 @@
 const lines = require('fs').readFileSync(0).toString().trim().split('\n');
 const n = Number(lines[0]);
-const A = lines[1].split(' ').map(Number).sort((a, b) => a - b);
-const B = lines[2].split(' ').map(Number).sort((a, b) => a - b);
+const A = lines[1].trim().split(' ').map(Number).sort((a, b) => a - b);
+const B = lines[2].trim().split(' ').map(Number).sort((a, b) => a - b);
 
 let flag = true;
 
