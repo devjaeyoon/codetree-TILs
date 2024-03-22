f2494528335138768bf7ea3b67422d36165bdb48
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-22 16:06:14 +0900

    Title: 벌금은 누구에게, Time: 190ms, Memory: 14MB, Status: Passed - CodeTree

--- 240322/벌금은 누구에게/who-will-pay.js
+++ 240322/벌금은 누구에게/who-will-pay.js
@@ -2,11 +2,15 @@ const lines = require('fs').readFileSync(0).toString().trim().split('\n');
 const [N, M, K] = lines[0].split(' ').map(Number);
 const studens = new Array(N).fill(0);
 
+let criminal = -1;
+
 for (let i = 1; i <= M; i++) {
   studens[Number(lines[i]) - 1] += 1;
   
   if (studens[Number(lines[i]) - 1] === K) {
-    console.log(lines[i]);
+    criminal = lines[i];
     break;
   }
 }
+
+console.log(criminal === -1 ? -1 : criminal)
