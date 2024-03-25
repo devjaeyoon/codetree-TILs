167a9500ef9b5f8f500712d738dd0d3a66d5f679
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-25 16:27:34 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 183ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240325/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.js
+++ 240325/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.js
@@ -6,30 +6,37 @@ if (N === 1) {
   }
 }
 
-if (N > 1) {
-    function generatePermutationsWithRepetition(n, k) {
-  let result = [];
-  let path = [];
+if (K === 1 && N !== 1) {
+  let str = '';
+  for (let i = 0; i < N; i++) {
+    str += '1 ';
+  }
+}
 
-  function backtrack() {
-    if (path.length == k) {
-      result.push([...path]);
-      return;
+if (N > 1) {
+  function generatePermutationsWithRepetition(n, k) {
+    let result = [];
+    let path = [];
+
+    function backtrack() {
+      if (path.length == k) {
+        result.push([...path]);
+        return;
+      }
+      for (let i = 1; i <= n; i++) {
+        path.push(i);
+        backtrack();
+        path.pop();
+      }
     }
-    for (let i = 1; i <= n; i++) {
-      path.push(i);
-      backtrack();
-      path.pop();
-    }
+
+    backtrack();
+    return result;
   }
 
-  backtrack();
-  return result;
-}
+  const result = generatePermutationsWithRepetition(K, N);
 
-const result = generatePermutationsWithRepetition(K, N);
-
-for (let i = 0; i < result.length; i++) {
-  console.log(result[i][0] + ' ' + result[i][1]);
-}
+  for (let i = 0; i < result.length; i++) {
+    console.log(result[i][0] + ' ' + result[i][1]);
+  }
 }
