e8fdc3420a6ab8b54c53b4631fc8a05d3737b389
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-25 16:15:56 +0900

    Title: k개 중에 1개를 n번 뽑기, Time: 236ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240325/k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition.js
@@ -0,0 +1,27 @@
+const [K, N] = require('fs').readFileSync(0).toString().split(' ').map(Number);
+
+function generatePermutationsWithRepetition(n, k) {
+  let result = [];
+  let path = [];
+
+  function backtrack() {
+    if (path.length == k) {
+      result.push([...path]);
+      return;
+    }
+    for (let i = 1; i <= n; i++) {
+      path.push(i);
+      backtrack();
+      path.pop();
+    }
+  }
+
+  backtrack();
+  return result;
+}
+
+const result = generatePermutationsWithRepetition(K, N);
+
+for (let i = 0; i < result.length; i++) {
+  console.log(result[i][0] + ' ' + result[i][1]);
+}
