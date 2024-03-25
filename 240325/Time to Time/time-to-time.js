165462a49feecb5db3845832a62bac8799ba0f30
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-03-25 16:43:28 +0900

    Title: Time to Time, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 240325/Time to Time/time-to-time.js
+++ 240325/Time to Time/time-to-time.js
@@ -2,11 +2,11 @@ const [a, b, c, d] = require('fs').readFileSync(0).toString().split(' ').map(Num
 let time = 0;
 
 if (b > d) {
-  time = d + 60 - b;
-  time = (c - a - 1) * 60;
+  time += d + 60 - b;
+  time += (c - a - 1) * 60;
 } else {
-  time = d - b;
-  time = (c - a) * 60;
+  time += d - b;
+  time += (c - a) * 60;
 }
 
 console.log(time);
