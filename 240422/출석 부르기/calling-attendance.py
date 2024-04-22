a54974ee9e59bf7198dd4de61a048d0c7e0127bf
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-04-22 13:53:44 +0900

    Title: 출석 부르기, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240422/출석 부르기/calling-attendance.py
@@ -0,0 +1,10 @@
+attendanceNumber = int(input())
+
+if attendanceNumber == 1:
+    print('John')
+elif attendanceNumber == 2:
+    print('Tom')
+elif attendanceNumber == 3:
+    print('Paul')
+else:
+    print('Vacancy')
