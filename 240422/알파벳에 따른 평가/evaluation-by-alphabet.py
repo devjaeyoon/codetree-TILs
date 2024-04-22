bea84bba9d329504db574692c8a4ff995aa652b6
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-04-22 14:32:54 +0900

    Title: 알파벳에 따른 평가, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240422/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+string = input()
+
+if string == 'S':
+    print('Superior')
+elif string == 'A':
+    print('Excellent')
+elif string == 'B':
+    print('Good')
+elif string == 'C':
+    print('Usually')
+elif string == 'D':
+    print('Effort')
+else:
+    print('Failure')
