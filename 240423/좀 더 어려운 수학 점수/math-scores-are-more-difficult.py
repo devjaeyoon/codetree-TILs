f91af3e31009601dda7146f86b6a99238f83bc12
이재윤 <131215937+devjaeyoon@users.noreply.github.com>
2024-04-23 09:34:39 +0900

    Title: 좀 더 어려운 수학 점수, Time: 123ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240423/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,12 @@
+mathA, engA = map(int, input().split())
+mathB, engB = map(int, input().split())
+
+if mathA > mathB:
+    print('A')
+elif mathA == mathB:
+    if engA > engB:
+        print('A')
+    else:
+        print('B')
+else:
+    print('B')
